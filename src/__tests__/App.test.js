dfb8c6f5d3887045c4d01ca28400752b479bbd2c
Daniel Garcia <daniel.of@gmail.com>
2020-05-22 13:49:33 -0300

    improving example test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-test('renders home page', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText('Home Page');
-  expect(linkElement).toBeInTheDocument();
+describe('Complete App', () => {
+  it('should render home page', () => {
+    const { getByTestId } = render(<App />);
+    const pageH1 = getByTestId('id-teste');
+    // fireEvent.click(linkElement);
+    expect(pageH1).toBeInTheDocument();
+  });
+
+  it('should change h1', () => {
+    const { getByText, getByTestId } = render(<App />);
+    const pageH1 = getByTestId('id-teste');
+    expect(pageH1).toContainElement(getByText('Home Page'));
+    const button = getByTestId('meu-botao');
+    fireEvent.click(button);
+    expect(pageH1).toContainElement(getByText('Bem Vindo!'));
+  });
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+function handleClick() {
+  const h1 = document.querySelector('h1');
+  h1.innerHTML = 'Bem Vindo!';
+}
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1 data-testid="id-teste">Home Page</h1>
+      <button data-testid="meu-botao" type="button" onClick={handleClick}>
+        Click Me!
+      </button>
+    </>
+  );
 }
 
 export default Home;
